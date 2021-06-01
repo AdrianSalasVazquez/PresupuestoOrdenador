635f7af61aeade62380251c5a5cce21430d20b51
Samuel <samualonso4@gmail.com>
2021-06-01 10:04:20 +0200

    Agregadas Bateria y MemoriaRam

---
+++ PresupuestoOrdenador/src/componentes/Bateria.java
@@ -0,0 +1,45 @@
+package componentes;
+
+public class Bateria extends Componente {
+	int voltaje;
+	String tipoPila;
+	int carga;
+	
+	public Bateria(String marca, String modelo, double precio, int voltaje, String tipoPila, int carga) {
+		super(marca, modelo, precio);
+		this.voltaje = voltaje;
+		this.tipoPila = tipoPila;
+		this.carga = carga;
+	}
+
+	public int getVoltaje() {
+		return voltaje;
+	}
+
+	public void setVoltaje(int voltaje) {
+		this.voltaje = voltaje;
+	}
+
+	public String getTipoPila() {
+		return tipoPila;
+	}
+
+	public void setTipoPila(String tipoPila) {
+		this.tipoPila = tipoPila;
+	}
+
+	public int getCarga() {
+		return carga;
+	}
+
+	public void setCarga(int carga) {
+		this.carga = carga;
+	}
+
+	@Override
+	public String toString() {
+		return null;
+	}
+	
+	
+}
---
+++ PresupuestoOrdenador/src/componentes/MemoriaRam.java
@@ -0,0 +1,55 @@
+package componentes;
+
+public class MemoriaRam extends Componente {
+	int numModulos;
+	int tamano;
+	String tipoMemoria;
+	int velocidad;
+	
+	public MemoriaRam(String marca, String modelo, double precio, int numModulos, int tamano, String tipoMemoria, int velocidad) {
+		super(marca, modelo, precio);
+		this.numModulos = numModulos;
+		this.tamano = tamano;
+		this.tipoMemoria = tipoMemoria;
+		this.velocidad = velocidad;
+	}
+
+	public int getNumModulos() {
+		return numModulos;
+	}
+
+	public void setNumModulos(int numModulos) {
+		this.numModulos = numModulos;
+	}
+
+	public int getTamano() {
+		return tamano;
+	}
+
+	public void setTamano(int tamano) {
+		this.tamano = tamano;
+	}
+
+	public String getTipoMemoria() {
+		return tipoMemoria;
+	}
+
+	public void setTipoMemoria(String tipoMemoria) {
+		this.tipoMemoria = tipoMemoria;
+	}
+
+	public int getVelocidad() {
+		return velocidad;
+	}
+
+	public void setVelocidad(int velocidad) {
+		this.velocidad = velocidad;
+	}
+
+	@Override
+	public String toString() {
+		return null;
+	}
+	
+	
+}
